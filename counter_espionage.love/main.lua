d98b87aff02293204d9064e26fad553fe3b8200b
Galen Hutchison <blkfinch@gmail.com>
2018-08-29 11:53:11 -0600

    random enemy location

--- counter_espionage.love/main.lua
+++ counter_espionage.love/main.lua
@@ -3,6 +3,7 @@ font = love.graphics.setNewFont( "compFont.ttf", 12 )
 love.graphics.setColor(0, 1, 0)
 
 Player = {}
+Enemy = {}
 
 -- leaving this here for reference change values to change window size
 -- window = love.window.setMode(300, 300)
@@ -10,6 +11,11 @@ Player = {}
 -- init values here
 function love.load()
   Player.bank = 0
+  randomize_enemy()
+end
+
+function randomize_enemy( ... )
+  Enemy.id = love.math.random(12)
 end
 
 -- update values here
@@ -19,14 +25,24 @@ function love.update()
   if love.keyboard.isDown("escape") then love.window.close() end
 end
 
--- for testing 
+-- for testing
 function love.keypressed(key)
   if key == "space" then
     Player.bank = Player.bank + 1
   end
+  if key == "r" then
+    randomize_enemy()
+  end
+end
+
+function debug_info( ... )
+  return "Enemy Location: " .. Enemy.id
 end
 
 -- draws to screen once per step
 function love.draw( ... )
-  love.graphics.print(Player.bank, 0, 0)
+  love.graphics.print("Bank: " .. Player.bank, 0, 0)
+
+  -- debug info
+  love.graphics.print(debug_info(), 0, 100)
 end
