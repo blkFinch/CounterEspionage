98c9e0264625c98a1c7b378d85a8185f16c64d22
KERRINGTON\galen <blkfinch@gmail.com>
2018-09-08 22:48:52 -0600

    working intro text scroll

--- counter_espionage.love/states/intro/main.lua
+++ counter_espionage.love/states/intro/main.lua
@@ -1,9 +1,23 @@
 
 function load( ... )
   file = love.filesystem.newFile("states/intro/intro.txt", "r")
-  text = { file:read(all) }
+  text = file:read(all)
+  substring = " "
+  i = 1
+  delay = 10
+end
+
+function love.update( ... )
+  delay = delay - 1
+  if(delay == 0 and i <= #text )then
+    delay = 10
+    substring = string.sub(text,1,i)
+    i = i + 1
+  end
+
+  if love.keyboard.isDown("space") then loadState("mainGame") end
 end
 
 function love.draw()
-  love.graphics.print(text,0,0)
+  love.graphics.print(substring , 30, 40)
 end
